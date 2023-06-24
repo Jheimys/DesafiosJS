44694a479ce558a6db7a7ba58b009d331c3fb6b7
Jheimys Bassani <jheimysbb@gmail.com>
2023-06-24 18:11:19 +0100

    inverter string

---
+++ Modulo03/3-maxMin/maxMin.js
@@ -0,0 +1,10 @@
+
+function maxMin(m,n) {
+    if(m > n) {
+       return  console.log(`${m} é maior que ${n}`)
+    } else {
+        return  console.log(`${m} é menor que ${n}`)
+    }
+}
+
+maxMin(3,2)
---
+++ Modulo03/4-reverseString/resverseString.js
@@ -0,0 +1,8 @@
+
+function reverse(text) {
+   let invertido =  text.split('').reverse().join('')
+
+   console.log(invertido)  
+}
+
+reverse('vai tomate cru')
